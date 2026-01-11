37668e331ee9c9f2915aaafd1cab1fba85ae02b7
santosh <santoshkumar90101s@gmail.com>
2026-01-11 22:49:57 +0530

    delete user

--- task-manager/src/users/users.controller.ts
+++ task-manager/src/users/users.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Req, UseGuards } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @UseGuards(AuthGuard('jwt'))
+  @Delete('me')
+  deleteAccount(@Req() req) {
+    return this.usersService.delete(req.user.userId);
+  }
+}
--- task-manager/src/users/users.service.ts
+++ task-manager/src/users/users.service.ts
@@ -18,10 +18,17 @@ export class UsersService {
   }
 
   update(id: number, data: any) {
-  return this.prisma.user.update({
-    where: { id },
-    data,
-  });
-}
+    return this.prisma.user.update({
+      where: { id },
+      data,
+    });
+  }
 
+  async delete(id: number) {
+    return this.prisma.client.$transaction([
+      this.prisma.task.deleteMany({ where: { userId: id } }),
+      this.prisma.note.deleteMany({ where: { userId: id } }),
+      this.prisma.user.delete({ where: { id } }),
+    ]);
+  }
 }
